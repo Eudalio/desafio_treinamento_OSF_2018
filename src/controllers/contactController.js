af6c6f7ddbc2d0e447d4769dada634fee72901f2
eudalio <tonnydallios@gmail.com>
2018-11-22 07:48:20 -0300

    Segunda parte. POST via formulário

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -3,10 +3,34 @@ const url = 'https://cryptic-retreat-41638.herokuapp.com/api/contacts';
 
 module.exports = {
 	async show (req, res) {
-		request.get(url, (error, response, body) => {
+		await request.get(url, (error, response, body) => {
 			let dados = JSON.parse(body)
 			const { data } = dados
+			console.log(data)
 			return res.render('contacts/show', { contacts: data })
 		})
+	},
+	renderForm (req, res) {
+		return res.render('contacts/register');
+	},
+	async store (req, res) {
+		const { name, email, phone, gender } = req.body
+
+		var content = {
+			"name": name,
+			"email": email,
+			"phone": phone,
+			"gender": gender
+		}
+
+		await request.post({
+			uri: url,
+			headers:{'content-type': 'application/x-www-form-urlencoded'},
+			body:require('querystring').stringify(content)},
+			(err, response, body) => {
+					console.log(body);
+					console.log(response.statusCode);
+					this.show(req, res)
+			});
 	}
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const contactController = require('./controllers/contactController');
 const routes = express.Router();
 
 routes.get('/contacts', contactController.show);
+routes.get('/contacts/create', contactController.renderForm);
+routes.post('/contacts', contactController.store);
 
 module.exports = routes;
